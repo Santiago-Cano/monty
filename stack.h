99a9f25bb869a49961458f4c230c4486cd33c679
Santiago Cano <2704@holbertonschool.com>
2021-12-04 23:16:41 -0500

    add stack.h

---
+++ stack.h
@@ -0,0 +1,26 @@
+#ifndef STACK_H
+#define STACK_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+stack_t *add_node_stack(stack_t **stack, int number);
+int delete_node_stack(stack_t **stack);
+void free_stack(stack_t **stack);
+#endif
